aa616d7fb0e1cbf90e1d5902099dd3613c97a451
m1z1na <51057800+m1z1na@users.noreply.github.com>
2022-09-15 20:38:41 +0300

    refactoring 15/09/22

--- equi/src/main/java/com/example/equi/model/Client.java
+++ equi/src/main/java/com/example/equi/model/Client.java
@@ -1,17 +1,14 @@
 package com.example.equi.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Client {
     private Integer id;
     private String name;
     private Integer amount;
-
-    public Integer getCost() {
-        return cost;
-    }
-
-    public void setCost(Integer cost) {
-        this.cost = cost;
-    }
 
     private Integer cost;
     private String waers;
@@ -23,48 +20,6 @@ public class Client {
         this.markup = markup;
         this.amount = amount;
         this.cost = cost;
-        this.name = name;
-    }
-
-    public String getWaers() {
-        return waers;
-    }
-
-    public void setWaers(String waers) {
-        this.waers = waers;
-    }
-
-    public Integer getMarkup() {
-        return markup;
-    }
-
-    public void setMarkup(Integer markup) {
-        this.markup = markup;
-    }
-
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
